fbde7904fb905c9106571ebc7ec1dcc807b768f5
Kevin Garcia <kevingarciasce@gmail.com>
2025-07-05 19:31:22 -0600

    POO

---
+++ src/Principal.java
@@ -0,0 +1,26 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+//        Pelicula otraPelicula = new Pelicula();
+//        otraPelicula.nombre = "Matrix";
+//        otraPelicula.fechaDeLanzamiento = 1998;
+//        otraPelicula.duracionEnMinutos = 180;
+
+//        otraPelicula.muestraFichaTecnica();
+
+
+    }
+}
